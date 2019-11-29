33f6ac0c18632a955c11a0a92e69866d97df2ac2
glass <glass.ships@outlook.com>
2019-11-28 22:02:58 -0700

    rearrange aliases

--- config.fish
+++ config.fish
@@ -1,29 +1,39 @@
-## Aliases and functions
+### Aliases and functions
+
+# general
 alias ll='ls -alF'
 
 alias env='env | sort'
 
+alias pls='sudo -E'
+
+alias howbig='du -csh $argv'
+
+alias upd='sudo apt update -y && sudo apt dist-upgrade -y'
+
+# git
 alias gs='git status'
 
-alias pls='sudo -E'
+# Docker
+alias docker-cleanup='docker rmi (docker images -f "dangling=true" -q)'
+
+alias centroot='docker run --rm -it -v ~/.ssh:$HOME/.ssh:ro -p 8888:8888 glasslabs/centroot:0.3'
 
 alias buildcdms='bash packages/cdms-jupyterlab/build.sh'
 
-alias howbig='du -csh $argv'
-alias upd='sudo apt update -y && sudo apt dist-upgrade -y'
-alias docker-cleanup='docker rmi (docker images -f "dangling=true" -q)'
-
+# move then ls
 function cs 
     cd $argv[1] 
     ls -al
 end
 
+# move, create directory if doesn't exist
 function move
     mkdir -p $argv[2]
     mv $argv[1] $argv[2]
 end
 
-## Options etc.
+### Options etc.
 
 set -e SSH_ASKPASS
 set -e GIT_ASKPASS
