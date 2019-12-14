e768dae15a9f883c0d6e23625f70dcec7437002b
glass <glass.ships@outlook.com>
2019-12-13 17:41:14 -0700

    rearrange

--- config.fish
+++ config.fish
@@ -1,6 +1,6 @@
-### Aliases and functions
+#### Aliases and functions
 
-# general
+## general
 alias ll='ls -alF'
 
 alias env='env | sort'
@@ -11,31 +11,36 @@ alias howbig='du -csh $argv'
 
 alias upd='sudo apt update -y && sudo apt dist-upgrade -y'
 
-# git
+## git
 alias gs='git status'
 
-# Docker
+## Docker
 alias docker-cleanup='docker rmi (docker images -f "dangling=true" -q)'
 
 alias centroot='docker run --rm -it -v ~/.ssh:$HOME/.ssh:ro -p 8888:8888 glasslabs/centroot:0.3'
 
+## misc
+alias clean-awk="bash -c 'rm -rf **/*~ **/__pycache__ build dist *.egg-info awkward1/*.so **/*.pyc'"
+
 alias buildcdms='bash packages/cdms-jupyterlab/build.sh'
 
-# move then ls
+## move then ls
 function cs 
     cd $argv[1] 
+    pwd
     ls -al
 end
 
-# move, create directory if doesn't exist
+## move, create directory if doesn't exist
 function move
     mkdir -p $argv[2]
     mv $argv[1] $argv[2]
 end
 
-### Options etc.
+#### Options etc.
 
 set -e SSH_ASKPASS
 set -e GIT_ASKPASS
 
-source  /opt/anaconda3/etc/fish/conf.d/conda.fish
+source /opt/anaconda3/etc/fish/conf.d/conda.fish
+conda activate base
