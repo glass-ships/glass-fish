30381d4d5f92189debda7144b182093dae97849c
glass <glass.ships@outlook.com>
2019-12-19 16:57:30 -0700

    rearrange again

--- config.fish
+++ config.fish
@@ -1,6 +1,15 @@
+#### Options etc.
+
+# force command-line password entry 
+set -e SSH_ASKPASS
+set -e GIT_ASKPASS
+
+source /opt/anaconda3/etc/fish/conf.d/conda.fish
+conda activate base
+
 #### Aliases and functions
 
-## general
+### general
 alias ll='ls -alF'
 
 alias env='env | sort'
@@ -11,36 +20,33 @@ alias howbig='du -csh $argv'
 
 alias upd='sudo apt update -y && sudo apt dist-upgrade -y'
 
-## git
+### git
 alias gs='git status'
 
-## Docker
+### Docker
 alias docker-cleanup='docker rmi (docker images -f "dangling=true" -q)'
+
+alias build-cdms='bash $HOME/dev/cdms-jupyterlab/build.sh'
 
 alias centroot='docker run --rm -it -v ~/.ssh:$HOME/.ssh:ro -p 8888:8888 glasslabs/centroot:0.3'
 
-## misc
-alias clean-awk="bash -c 'rm -rf **/*~ **/__pycache__ build dist *.egg-info awkward1/*.so **/*.pyc'"
+### misc
+alias build-test='rm -rf ./* ;; g++ -I/opt/awkward-1.0/include -L/opt/awkward-1.0/build/lib.linux-x86_64-3.7/awkward1 ../tests/test.cpp -lawkward-static -lawkward-cpu-kernels-static -o Test ;; ./Test'
 
-alias buildcdms='bash packages/cdms-jupyterlab/build.sh'
+alias build-cleanup="bash -c 'rm -rf **/*~ **/__pycache__ build dist *.egg-info awkward1/*.so **/*.pyc'"
 
-## move then ls
-function cs 
-    cd $argv[1] 
+alias cmake-try='cd build ;; rm -rf ./* ;; cmake .. ;; make'
+
+# cd and ls
+function cs
+    cd $argv[1]
     pwd
     ls -al
 end
 
-## move, create directory if doesn't exist
+# mv, create dir if DNE
 function move
     mkdir -p $argv[2]
     mv $argv[1] $argv[2]
 end
 
-#### Options etc.
-
-set -e SSH_ASKPASS
-set -e GIT_ASKPASS
-
-source /opt/anaconda3/etc/fish/conf.d/conda.fish
-conda activate base
