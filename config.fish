b43c3c0e13840819044bd3f12e08cdbb92ba9516
glass <glass.ships@outlook.com>
2020-06-12 14:53:13 -0600

    idk, something new prob

--- config.fish
+++ config.fish
@@ -4,35 +4,31 @@
 set -e SSH_ASKPASS
 set -e GIT_ASKPASS
 
-source /opt/anaconda3/etc/fish/conf.d/conda.fish
-conda activate base
-
 #### Aliases and functions
-
-### general
+### general cli
 alias ll='ls -alF'
 
 alias env='env | sort'
 
-alias pls='sudo -E'
+alias please='sudo -E'
 
 alias howbig='du -csh $argv'
 
 alias upd='sudo apt update -y && sudo apt dist-upgrade -y'
 
 ### git
-
 alias gs='git status'
 
-### Docker
+### docker
+alias docker-rm-containers='docker rm (docker ps -a -q)'
 
 alias docker-cleanup='docker rmi (docker images -f "dangling=true" -q)'
 
 alias build-cdms='bash $HOME/dev/cdms-jupyterlab/build.sh'
 
-alias centroot='docker run --rm -it -v ~/.ssh:$HOME/.ssh:ro -p 8888:8888 glasslabs/centroot:0.3'
+alias centroot='docker run --rm -it -v ~/.ssh:$HOME/.ssh:ro -p 8888:8888 glasslabs/centroot:latest'
 
-### misc
+### misc chains and functions
 
 alias cleanup="bash -c 'rm -rf **/*~ **/__pycache__ build dist *.egg-info awkward1/*.so **/*.pyc'"
 
@@ -45,21 +41,28 @@ function cs
   ls -al
 end
 
-# build cpp (with awkward)
-function build-cpp
-  if cd build
-    rm -rf ./*
-    g++ -I/opt/awkward-1.0/include -L/opt/awkward-1.0/build/lib.linux-x86_64-3.7/awkward1 ../tests/test_awkward_toy.cpp -lawkward-static -lawkward-cpu-kernels-static -o Test_Awkward_Toy 
-    ./Test_Awkward_Toy
-  else
-    echo "Please use \'build\' directory"
-    exit
-  end
-end
-
 # mv, create dir if DNE
 function move
-    mkdir -p $argv[2]
-    mv $argv[1] $argv[2]
+  mkdir -p $argv[3]
+  mv $argv[2] $argv[3]
 end
 
+# build cpp (with awkward)
+function gpp-awk
+  g++ -I/opt/awkward-1.0/include -L/opt/awkward-1.0/build/lib.linux-x86_64-3.7/awkward1 $argv[1] -lawkward-static -lawkward-cpu-kernels-static -o $argv[2]
+end
+
+# rebuild kaitai-struct-compiler
+function build-ksc
+  cd ~/dev/awkward-kaitai/kaitai_struct_compiler
+  if sbt compilerJVM/debian:packageBin
+    pls dpkg -i ~/dev/awkward-kaitai/kaitai_struct_compiler/jvm/target/kaitai-struct-compiler_0.9-SNAPSHOT_all.deb
+  else
+    echo 'ksc failed to compile!'
+  end
+  cd -
+end
+
+######
+source /opt/anaconda3/etc/fish/conf.d/conda.fish
+conda activate base
