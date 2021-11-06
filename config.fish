80ea29b4b765c684130c71e39edd402b1f1ade4f
glass <glass.ships@outlook.com>
2021-11-06 00:38:48 -0600

    test gc alias

--- config.fish
+++ config.fish
@@ -54,7 +54,7 @@ alias jlab='jupyter lab --no-browser'
 
 alias gs='git status'
 
-alias bfg='java -jar /home/glass/dev/bfg-1.13.1.jar'
+alias gc='[ "`git log --pretty=%H ...refs/heads/master^ | head -n 1`" = "`git ls-remote origin -h refs/heads/master |cut -f1`" ] ;; echo "up to date" || echo "not up to date"'
 
 ### docker aliases
 
