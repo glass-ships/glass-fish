a968563995392bad71ebe58a4883d50dc2d2abb9
glass-ships <glass.ships@outlook.com>
2024-08-07 13:02:04 -0600

    replace home with $HOME

--- config.fish
+++ config.fish
@@ -106,13 +106,13 @@ set -e DIR
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-if test -f /home/ge2/miniforge3/bin/conda
-    eval /home/ge2/miniforge3/bin/conda "shell.fish" "hook" $argv | source
+if test -f $HOME/miniforge3/bin/conda
+    eval $HOME/miniforge3/bin/conda "shell.fish" "hook" $argv | source
 else
-    if test -f "/home/ge2/miniforge3/etc/fish/conf.d/conda.fish"
-        . "/home/ge2/miniforge3/etc/fish/conf.d/conda.fish"
+    if test -f "$HOME/miniforge3/etc/fish/conf.d/conda.fish"
+        . "$HOME/miniforge3/etc/fish/conf.d/conda.fish"
     else
-        set -x PATH "/home/ge2/miniforge3/bin" $PATH
+        set -x PATH "$HOME/miniforge3/bin" $PATH
     end
 end
 # <<< conda initialize <<<
