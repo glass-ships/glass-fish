a1fd41aa8c6b6cd002f093e163a301b6a4f1dd77
glass-ships <glass.ships@outlook.com>
2024-09-04 11:03:30 -0400

    add vpn fn

--- functions/ornl.fish
+++ functions/ornl.fish
@@ -2,3 +2,26 @@
 
 abbr -a cdmantid 'cd ~/dev/ornl/mantid'
 abbr -a cdlds 'cd ~/dev/ornl/live_data_server'
+abbr -a cdref 'cd ~/dev/ornl/refl1d'
+
+
+function vpn -d "Connect/Disconnect to ORNL VPN/show status"
+    if test (count $argv) -ne 1
+        echo "Usage: vpn [connect|disconnect|status]"
+        return 1
+    end
+
+    if [ $argv[1] = connect ]
+        echo "Connecting to ORNL VPN..."
+        globalprotect connect -p vpn.ornl.gov
+    else if [ $argv[1] = disconnect ]
+        echo "Disconnecting from ORNL VPN..."
+        globalprotect disconnect
+    else if [ $argv[1] = status ]
+        echo "VPN Status:"
+        globalprotect show --status
+    else
+        echo "Usage: vpn [connect|disconnect|status]"
+        return 1
+    end
+end
