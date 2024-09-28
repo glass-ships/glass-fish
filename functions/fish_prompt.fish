d2ffb3baf197b53e3fa9dc0486a2d0450ae693c0
glass-ships <glass.ships@outlook.com>
2024-09-27 19:26:59 -0600

    modify prompt slightly

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,59 +1,60 @@
 # Customize FISH Prompt
 function fish_prompt
-  # remove welcome message
-  set -U fish_greeting
-  # $status gets nuked as soon as something else is run, e.g. set_color
-  # so it has to be saved asap.
-  set -l last_status $status
+    # remove welcome message
+    set -U fish_greeting
+    # $status gets nuked as soon as something else is run, e.g. set_color
+    # so it has to be saved asap.
+    set -l last_status $status
 
-  ### Git stuff
-  set normal (set_color normal)
-  set magenta (set_color magenta)
-  set yellow (set_color yellow)
-  set green (set_color green)
-  set red (set_color red)
-  set gray (set_color -o black)
-  
-  # Fish git prompt
-  set __fish_git_prompt_showdirtystate 'yes'
-  set __fish_git_prompt_showstashstate 'yes'
-  set __fish_git_prompt_showuntrackedfiles 'yes'
-  set __fish_git_prompt_showupstream 'yes'
-  set __fish_git_prompt_color_branch yellow
-  set __fish_git_prompt_color_upstream_ahead green
-  set __fish_git_prompt_color_upstream_behind red
-  
-  # Status Chars
-  set __fish_git_prompt_char_dirtystate '⚡'
-  set __fish_git_prompt_char_stagedstate '→'
-  set __fish_git_prompt_char_untrackedfiles '☡'
-  set __fish_git_prompt_char_stashstate '↩'
-  set __fish_git_prompt_char_upstream_ahead '+'
-  set __fish_git_prompt_char_upstream_behind '-'
-   
-  # set some colors
-  set -g cp (set_color 8D3AF2) # purple
-  set -g c0 (set_color 4F81FF) # blue
-  set -g c1 (set_color 31FA3A) # green
-  set -g c2 (set_color F2E03A) # yellow
-  set -g c3 (set_color 34FFF3) # cyan
-  set -g c4 (set_color FF34DD) # pink
-  set -g ce (set_color $fish_color_error)
+    ### Git stuff
+    set normal (set_color normal)
+    set magenta (set_color magenta)
+    set yellow (set_color yellow)
+    set green (set_color green)
+    set red (set_color red)
+    set gray (set_color -o black)
 
-  # Clear the line because fish seems to emit the prompt twice. The initial
-  # display, then when you press enter.
-  printf "\033[K"
+    # Fish git prompt
+    set -g __fish_git_prompt_showdirtystate yes
+    set -g __fish_git_prompt_showstashstate yes
+    set -g __fish_git_prompt_showuntrackedfiles yes
+    set -g __fish_git_prompt_showupstream yes
+    set -g __fish_git_prompt_color_branch yellow
+    set -g __fish_git_prompt_color_upstream_ahead green
+    set -g __fish_git_prompt_color_upstream_behind red
 
-  ### construct prompt
-  # timestamp 
-  printf "$cp\n["(date "+$c2%H:%M")"$cp]"
-  # current dir
-  printf (pwd | sed "s,/,$c2/$c3,g") 
-  # git status
-  printf "$c4%s" (__fish_git_prompt)
-  # user and host in a fish
-  printf "\n><$c3"(whoami)"$c4@$c3"(hostname)"$c4> "
+    # Status Chars
+    set -g __fish_git_prompt_char_dirtystate '⚡'
+    set -g __fish_git_prompt_char_stagedstate '→'
+    set -g __fish_git_prompt_char_untrackedfiles '☡'
+    set -g __fish_git_prompt_char_stashstate '↩'
+    set -g __fish_git_prompt_char_upstream_ahead '+'
+    set -g __fish_git_prompt_char_upstream_behind -
 
-  set_color normal
+    # set some colors
+    set -g PURPLE (set_color 8D3AF2)
+    set -g BLUE (set_color 4F81FF)
+    set -g GREEN (set_color 31FA3A)
+    set -g YELLOW (set_color F2E03A)
+    set -g CYAN (set_color 34FFF3)
+    set -g PINK (set_color FF34DD)
+    set -g WHITE (set_color FFFFFF)
+    set -g ce (set_color $fish_color_error)
+
+    # Clear the line because fish seems to emit the prompt twice,
+    # i.e. the initial display, then when you press enter.
+    printf "\033[K"
+
+    ### construct prompt
+    # timestamp 
+    # printf "$PURPLE\n["(date "+$YELLOW%H:%M")"$PURPLE]"
+    printf "$PURPLE\n["(date "+$YELLOW%Y-%m-%d %H:%M")"$PURPLE]"
+    # current dir
+    printf "%s$WHITE" (pwd | sed "s,/,$YELLOW/$CYAN,g")
+    # git status
+    printf (__fish_git_prompt)
+    # user and host in a fish
+    printf "\n$PINK><$CYAN"(whoami)"$PINK@$CYAN"(hostname)"$PINK> "
+
+    set_color normal
 end
-
