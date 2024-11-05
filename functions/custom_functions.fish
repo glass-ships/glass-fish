24b5c01d6abb8c28cd5ae628905cc37b9d993ad3
glass-ships <glass.ships@outlook.com>
2024-11-05 11:20:32 -0500

    add miniforge convenience functions

--- functions/custom_functions.fish
+++ functions/custom_functions.fish
@@ -1,20 +1,15 @@
 #!/usr/bin/env fish
 
+function trim -d "trim whitespace (leading and trailing only)"
+    echo -n (string trim -r -l $argv)
+end
+
+
 function cs -d "cd and ls (with auto conda activation)"
     cd $argv[1]
     ls -al
 end
 
-function conda_activate --on-variable PWD -d "activate conda env"
-    if [ -f (pwd)/.condaconfig ]
-        set -gx CONDACONFIGDIR (pwd)
-        conda activate (cat .condaconfig)
-    end
-    if not [ (string match "*$CONDACONFIGDIR*" (pwd)) ]
-        set -e CONDACONFIGDIR
-        conda deactivate
-    end
-end
 
 function move -d "mv, create dir if DNE"
     set destination $argv[-1]
@@ -78,7 +73,7 @@ function git-cleanup-branches -d "git delete local branches not on remote"
 end
 
 function gl-clone -d "gitlab clone with personal token"
-    set -l options "o/ornl"
+    set -l options o/ornl
     argparse $options -- $argv
 
     if set --query _flag_ornl
@@ -112,4 +107,46 @@ function update-cloudflared -d "update cloudflared"
     wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
     sudo dpkg -i cloudflared-linux-amd64.deb
     rm cloudflared-linux-amd64.deb
+end
+
+### Conda stuff
+
+function conda_activate --on-variable PWD -d "activate conda env"
+    if [ -f (pwd)/.condaconfig ]
+        set -gx CONDACONFIGDIR (pwd)
+        conda activate (cat .condaconfig)
+    end
+    if not [ (string match "*$CONDACONFIGDIR*" (pwd)) ]
+        set -e CONDACONFIGDIR
+        conda deactivate
+    end
+end
+
+function install-miniforge -d "install miniforge"
+    # Install Miniforge
+    wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-(uname)-(uname -m).sh
+    bash Miniforge3-(uname)-(uname -m).sh
+end
+
+function uninstall-miniforge -d "uninstall miniforge"
+    if not [ (count $argv) -eq 0 ] && [ $argv[1] = "check" ]
+        conda init --reverse --dry-run
+        return
+    else
+        conda init --reverse
+    end
+    set CONDA_BASE_ENVIRONMENT (conda info --base)
+    echo The next command will delete all files in {$CONDA_BASE_ENVIRONMENT}.
+    set -l response (read -l -P "Continue? [y/n]")
+    if not string match -q "y*" -- string lower $response
+        echo "Aborting..."
+        return
+    end
+    echo Deleting {$CONDA_BASE_ENVIRONMENT}
+    rm -rf {$CONDA_BASE_ENVIRONMENT}
+    echo {$HOME}/.condarc will be removed if it exists
+    rm -f "{$HOME}/.condarc"
+    echo {$HOME}/.conda and underlying files will be removed if they exist.
+    rm -fr {$HOME}/.conda
+    echo "Uninstall complete."
 end
--- functions/ornl.fish
+++ functions/ornl.fish
@@ -1,9 +1,11 @@
 ### cd shortcuts ###
+
 abbr -a cdlds 'cd ~/dev/ornl/live_data_server'
 abbr -a cdmantid 'cd ~/dev/ornl/mantid'
 abbr -a cdref 'cd ~/dev/ornl/refl1d'
 abbr -a cdsans 'cd ~/dev/ornl/sans-backend'
 abbr -a cdwebref 'cd ~/dev/ornl/web_reflectivity'
+
 
 function mount-analysis -d "Mount the analysis server for the given instrument"
     if test (count $argv) -ne 1
@@ -16,6 +18,17 @@ function mount-analysis -d "Mount the analysis server for the given instrument"
     mkdir -p ~/dev/ornl/analysis/SNS/$inst
     sshfs ge2@analysis.sns.gov:/SNS/$inst/ ~/dev/ornl/analysis/SNS/$inst # -o defer_permissions,volname=$inst
 end
+
+function unmount-analysis -d "Unmount the mountpoint for analysis server for given instrument"
+    if test (count $argv) -ne 1
+        echo "Usage: unmount-analysis <instrument>"
+        return 1
+    end
+
+    set inst $argv[1]
+    echo "Unmounting analysis server for $inst..."
+end
+
 
 function vpn -d "Connect/Disconnect to ORNL VPN/show status"
     if test (count $argv) -ne 1
